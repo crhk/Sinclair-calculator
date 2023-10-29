8749e4489c20d2974ebc0b2f62dc8906c99c7ef1
Mathieu Tang <mathieu.tang@pretto.fr>
2023-10-29 17:43:54 +0100

    sample de test

---
+++ src/app/test/data/sample1.ts
@@ -0,0 +1,247 @@
+export const sample1 = [
+  {
+    team: { name: "CHM PLOUHINEC POINTE DU RAZ", total: "603,65" },
+    players: [
+      {
+        name: "QUERE Noémie",
+        bw: 61.6,
+        snatches: [
+          { weight: 51, status: "good lift" },
+          { weight: 54, status: "good lift" },
+          { weight: 56, status: "good lift" },
+        ],
+        cjs: [
+          { weight: 58, status: "good lift" },
+          { weight: 61, status: "good lift" },
+          { weight: -64, status: "no lift" },
+        ],
+        sex: "F",
+      },
+      {
+        name: "QUERE Eloïse",
+        bw: 66.2,
+        snatches: [
+          { weight: 54, status: "good lift" },
+          { weight: 57, status: "good lift" },
+          { weight: 60, status: "good lift" },
+        ],
+        cjs: [
+          { weight: 63, status: "good lift" },
+          { weight: 66, status: "good lift" },
+          { weight: 69, status: "good lift" },
+        ],
+        sex: "F",
+      },
+      {
+        name: "SCOARNEC Ambre",
+        bw: 79.4,
+        snatches: [
+          { weight: 50, status: "good lift" },
+          { weight: 55, status: "good lift" },
+          { weight: 59, status: "good lift" },
+        ],
+        cjs: [
+          { weight: 60, status: "good lift" },
+          { weight: 65, status: "good lift" },
+          { weight: 70, status: "good lift" },
+        ],
+        sex: "F",
+      },
+      {
+        name: "QUERE Romane",
+        bw: 53.2,
+        snatches: [
+          { weight: 35, status: "good lift" },
+          { weight: 38, status: "good lift" },
+          { weight: 40, status: "good lift" },
+        ],
+        cjs: [
+          { weight: 43, status: "good lift" },
+          { weight: 47, status: "good lift" },
+          { weight: 51, status: "good lift" },
+        ],
+        sex: "F",
+      },
+    ],
+  },
+  {
+    team: { name: "CROSSFIT BREST HALTEROPHILIE", total: "584,50" },
+    players: [
+      {
+        name: "TRIMECH Nourhene",
+        bw: 58.8,
+        snatches: [
+          { weight: 37, status: "good lift" },
+          { weight: 41, status: "good lift" },
+          { weight: 45, status: "good lift" },
+        ],
+        cjs: [
+          { weight: 53, status: "good lift" },
+          { weight: 57, status: "good lift" },
+          { weight: -61, status: "no lift" },
+        ],
+        sex: "F",
+      },
+      {
+        name: "GARRIC Juliette",
+        bw: 62.1,
+        snatches: [
+          { weight: 50, status: "good lift" },
+          { weight: 54, status: "good lift" },
+          { weight: 59, status: "good lift" },
+        ],
+        cjs: [
+          { weight: 65, status: "good lift" },
+          { weight: 70, status: "good lift" },
+          { weight: -74, status: "no lift" },
+        ],
+        sex: "F",
+      },
+      {
+        name: "WIPLIER Manon",
+        bw: 61.3,
+        snatches: [
+          { weight: 48, status: "good lift" },
+          { weight: -52, status: "no lift" },
+          { weight: -52, status: "no lift" },
+        ],
+        cjs: [
+          { weight: 60, status: "good lift" },
+          { weight: -65, status: "no lift" },
+          { weight: 65, status: "good lift" },
+        ],
+        sex: "F",
+      },
+      {
+        name: "DORVEAUX Chloé",
+        bw: 93.6,
+        snatches: [
+          { weight: 45, status: "good lift" },
+          { weight: -50, status: "no lift" },
+          { weight: 52, status: "good lift" },
+        ],
+        cjs: [
+          { weight: 55, status: "good lift" },
+          { weight: 61, status: "good lift" },
+          { weight: -64, status: "no lift" },
+        ],
+        sex: "F",
+      },
+    ],
+  },
+  {
+    team: { name: "RBC WORKOUT", total: "573,59" },
+    players: [
+      {
+        name: "MARAMOTTI Anna",
+        bw: 51.6,
+        snatches: [
+          { weight: 33, status: "good lift" },
+          { weight: 36, status: "good lift" },
+          { weight: -42, status: "no lift" },
+        ],
+        cjs: [
+          { weight: 48, status: "good lift" },
+          { weight: 51, status: "good lift" },
+          { weight: 54, status: "good lift" },
+        ],
+        sex: "F",
+      },
+      {
+        name: "CRENN Aurore",
+        bw: 57.8,
+        snatches: [
+          { weight: 48, status: "good lift" },
+          { weight: -52, status: "no lift" },
+          { weight: -52, status: "no lift" },
+        ],
+        cjs: [
+          { weight: 58, status: "good lift" },
+          { weight: 61, status: "good lift" },
+          { weight: -64, status: "no lift" },
+        ],
+        sex: "F",
+      },
+      {
+        name: "GARRO Maud",
+        bw: 58.6,
+        snatches: [
+          { weight: 45, status: "good lift" },
+          { weight: 48, status: "good lift" },
+          { weight: -50, status: "no lift" },
+        ],
+        cjs: [
+          { weight: 53, status: "good lift" },
+          { weight: 56, status: "good lift" },
+          { weight: 60, status: "good lift" },
+        ],
+        sex: "F",
+      },
+      {
+        name: "LAMIRAL Valentine",
+        bw: 69.7,
+        snatches: [
+          { weight: 43, status: "good lift" },
+          { weight: 46, status: "good lift" },
+          { weight: 49, status: "good lift" },
+        ],
+        cjs: [
+          { weight: 60, status: "good lift" },
+          { weight: 63, status: "good lift" },
+          { weight: -66, status: "no lift" },
+        ],
+        sex: "F",
+      },
+    ],
+  },
+  {
+    team: { name: "CA LORIENTAIS", total: "421,36" },
+    players: [
+      {
+        name: "CAILLE Titia",
+        bw: 73.4,
+        snatches: [
+          { weight: 44, status: "good lift" },
+          { weight: 48, status: "good lift" },
+          { weight: 51, status: "good lift" },
+        ],
+        cjs: [
+          { weight: -52, status: "no lift" },
+          { weight: 52, status: "good lift" },
+          { weight: 56, status: "good lift" },
+        ],
+        sex: "F",
+      },
+      {
+        name: "FERREC Énora",
+        bw: 62.7,
+        snatches: [
+          { weight: 60, status: "good lift" },
+          { weight: 63, status: "good lift" },
+          { weight: -65, status: "no lift" },
+        ],
+        cjs: [
+          { weight: 73, status: "good lift" },
+          { weight: 75, status: "good lift" },
+          { weight: -77, status: "no lift" },
+        ],
+        sex: "F",
+      },
+      {
+        name: "KERVIEL Morgane",
+        bw: 59.1,
+        snatches: [
+          { weight: 31, status: "good lift" },
+          { weight: 33, status: "good lift" },
+          { weight: -35, status: "no lift" },
+        ],
+        cjs: [
+          { weight: 45, status: "good lift" },
+          { weight: -48, status: "no lift" },
+          { weight: 48, status: "good lift" },
+        ],
+        sex: "F",
+      },
+    ],
+  },
+];
