0b394b452c59e3accf6e64fcc71bba03df9d4d34
Mathieu Tang <mathieu.tang@pretto.fr>
2023-10-29 18:06:23 +0100

    using id in params (to check with good connection)

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,199 @@
+"use client";
+
+import { Fragment, useEffect, useState } from "react";
+import { useTeams } from "../hooks/useTeams";
+import { calculateCJ } from "../lib/calculateCJ";
+import { Team } from "../data/types";
+import { calculateSinclair } from "../lib/sinclair";
+
+const getClassName = (status: string) => {
+  switch (status) {
+    case "good lift":
+      return "bg-green-600 font-semibold";
+    case "no lift":
+      return "bg-red-600";
+    case "to do":
+    default:
+      return "";
+  }
+};
+
+export default function Page({ params }: { params: { id: string } }) {
+  const [data, setData] = useState<Team[] | null>(null);
+  const { actualBestIWFTeam, projectedBestIWFTeam } = useTeams(data);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api", {
+        method: "POST",
+        body: JSON.stringify({ url: params.id }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await res.json();
+      setData(data);
+    };
+
+    fetchData();
+  }, [params.id]);
+
+  if (!data) {
+    return <>de la merde ton id</>;
+  }
+
+  return (
+    <>
+      <h1>LE CALCULATEUR DE LA VICTOIRE</h1>
+      <table className="text-center">
+        <thead>
+          <tr>
+            <th>Nom</th>
+            <th>PDC</th>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>Meilleur arraché</th>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>Meilleur EJ</th>
+            <th>Total</th>
+            <th>Points</th>
+            <th>Suggestion EJ pour battre le meilleur total actuel</th>
+            <th>Total projeté</th>
+            <th>Suggestion EJ pour battre le meilleur total projeté</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.length &&
+            data.map((elem) => {
+              const { team, players } = elem;
+              return (
+                <Fragment key={team.name}>
+                  <tr>
+                    <td colSpan={11} className="text-left font-extrabold">
+                      {team.name}
+                    </td>
+                    <td colSpan={2} className="text-left">
+                      {team.total}
+                    </td>
+                    <td className="text-left">
+                      {Math.floor(
+                        players.reduce(
+                          (current, { bw, snatches, cjs, sex }) => {
+                            const maxSnatch = Math.max(
+                              ...snatches
+                                .filter(
+                                  ({ status }) =>
+                                    status === "good lift" || status === "to do"
+                                )
+                                .map(({ weight }) => weight)
+                            );
+                            const maxCJ = Math.max(
+                              ...cjs
+                                .filter(
+                                  ({ status }) =>
+                                    status === "good lift" || status === "to do"
+                                )
+                                .map(({ weight }) => weight)
+                            );
+
+                            const total = maxSnatch + maxCJ;
+
+                            const sinclair = calculateSinclair(total, bw, sex);
+                            return current + sinclair;
+                          },
+                          0
+                        ) * 100
+                      ) / 100}
+                    </td>
+                  </tr>
+                  {players.map((player) => {
+                    const { name, bw, snatches, cjs, sex } = player;
+                    const maxSnatch = Math.max(
+                      ...snatches
+                        .filter(({ status }) => status === "good lift")
+                        .map(({ weight }) => weight),
+                      0
+                    );
+                    const maxCJ = Math.max(
+                      ...cjs
+                        .filter(({ status }) => status === "good lift")
+                        .map(({ weight }) => weight),
+                      0
+                    );
+
+                    const total = maxSnatch + maxCJ;
+
+                    const sinclair = calculateSinclair(total, bw, sex);
+
+                    return (
+                      <tr key={name}>
+                        <td>{name}</td>
+                        <td>{bw}</td>
+                        {snatches.map(({ weight, status }) => {
+                          return (
+                            <td
+                              key={`snatch-${weight}-${status}`}
+                              className={getClassName(status)}
+                            >
+                              {weight}
+                            </td>
+                          );
+                        })}
+                        <td className="font-extrabold">{maxSnatch}</td>
+                        {cjs.map(({ weight, status }) => {
+                          return (
+                            <td
+                              key={`cj-${weight}-${status}`}
+                              className={getClassName(status)}
+                            >
+                              {weight}
+                            </td>
+                          );
+                        })}
+                        <td className="font-extrabold">{maxCJ}</td>
+                        <td>{total}</td>
+                        <td>{sinclair}</td>
+                        <td>
+                          {cjs.filter(({ status }) => status === "to do")
+                            .length < 1
+                            ? "finito pipo"
+                            : calculateCJ(
+                                actualBestIWFTeam,
+                                elem,
+                                player,
+                                cjs.filter(
+                                  ({ status }) => status === "to do"
+                                )[0].weight
+                              )}
+                        </td>
+                        <td></td>
+                        <td>
+                          {cjs.filter(({ status }) => status === "to do")
+                            .length < 1
+                            ? "finito pipo"
+                            : calculateCJ(
+                                projectedBestIWFTeam,
+                                elem,
+                                player,
+                                cjs.filter(
+                                  ({ status }) => status === "to do"
+                                )[0].weight
+                              )}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </Fragment>
+              );
+            })}
+          <tr></tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,88 +2,107 @@ import { NextResponse } from "next/server";
 import puppeteer, { ElementHandle } from "puppeteer";
 
 const getStatus = (status: string) => {
-  switch(status) {
-    case 'attempt_1':
-      return 'good lift'
-    case 'attempt_2':
-      return 'no lift'
-    case 'attempt_0':
+  switch (status) {
+    case "attempt_1":
+      return "good lift";
+    case "attempt_2":
+      return "no lift";
+    case "attempt_0":
     default:
-      return 'to do'
+      return "to do";
   }
-}
+};
 
-const formatLifts = async (lifts: ElementHandle<HTMLTableCellElement>[]) => await Promise.all(lifts.map(async (lift: ElementHandle<HTMLTableCellElement>) => {
-  const liftDiv = await lift.$('div')
-  if (liftDiv) {
-    const liftStatus = await liftDiv.getProperty('className').then(cn => cn.jsonValue())
-    const liftNumber = await liftDiv.$eval('strong', node => node.innerText)
+const formatLifts = async (lifts: ElementHandle<HTMLTableCellElement>[]) =>
+  await Promise.all(
+    lifts.map(async (lift: ElementHandle<HTMLTableCellElement>) => {
+      const liftDiv = await lift.$("div");
+      if (liftDiv) {
+        const liftStatus = await liftDiv
+          .getProperty("className")
+          .then((cn) => cn.jsonValue());
+        const liftNumber = await liftDiv.$eval(
+          "strong",
+          (node) => node.innerText
+        );
 
-    if (liftStatus && liftNumber) {
-      return {
-        weight: parseInt(liftNumber),
-        status: getStatus(liftStatus)
+        if (liftStatus && liftNumber) {
+          return {
+            weight: parseInt(liftNumber),
+            status: getStatus(liftStatus),
+          };
+        }
       }
-    }
-  }
-}))
+    })
+  );
 
 export async function POST(request: Request) {
-  const { url } = await request.json()
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const { url } = await request.json();
+  const browser = await puppeteer.launch({ headless: "new" });
 
   // Create a page
   const page = await browser.newPage();
 
   // Go to your site
-  await page.goto(`http://scoresheet.ffhaltero.fr/scoresheet/team/competition/view/${url}`);
+  await page.goto(
+    `http://scoresheet.ffhaltero.fr/scoresheet/team/competition/view/${url}`
+  );
 
-  const isClosed = await page.$('#printButton')
-  const resultTable = await page.$(`.event-box ${isClosed ? 'tbody' : 'table' }`)
+  const isClosed = await page.$("#printButton");
+  const resultTable = await page.$(
+    `.event-box ${isClosed ? "tbody" : "table"}`
+  );
 
-  const data: any[] = []
+  const data: any[] = [];
 
   if (resultTable) {
-    const allRows = await resultTable.$$(`${isClosed ? 'tr' : 'tbody' }`)
+    const allRows = await resultTable.$$(`${isClosed ? "tr" : "tbody"}`);
     if (allRows) {
       for (const row of allRows) {
-        await row.$eval('div > font > strong', n => n.innerText)
-        .then(async (teamName) => {
-            const teamPoints = await row.$eval('td > font > strong', n => n.innerText).catch(error => console.error(error.message));
+        await row
+          .$eval("div > font > strong", (n) => n.innerText)
+          .then(async (teamName) => {
+            const teamPoints = await row
+              .$eval("td > font > strong", (n) => n.innerText)
+              .catch((error) => console.error(error.message));
             data.push({
               team: {
                 name: teamName,
-                total: teamPoints
+                total: teamPoints,
               },
-              players: []
-            })
-        })
-        .catch(async (error) => {
-          const player = await row.$$('td')
-          
-          if (player && player.length) {
-            const name = await (await player[1].getProperty('innerText')).jsonValue()
-            const bw = await (await player[5].getProperty('innerText')).jsonValue()
-            const sex = await (await player[16].getProperty('innerText')).jsonValue()
-            const formatPlayer = {
-              name,
-              bw: parseFloat(bw.replace(',', '.')),
-              snatches: await formatLifts(player.slice(6, 9)),
-              cjs: await formatLifts(player.slice(10, 13)),
-              sex: sex.includes('M') ? 'M' : 'F'
-            }
+              players: [],
+            });
+          })
+          .catch(async (error) => {
+            const player = await row.$$("td");
 
-            const lastIndex = data.length - 1
-            data[lastIndex].players.push(formatPlayer)
-          }
-        });
+            if (player && player.length) {
+              const name = await (
+                await player[1].getProperty("innerText")
+              ).jsonValue();
+              const bw = await (
+                await player[5].getProperty("innerText")
+              ).jsonValue();
+              const sex = await (
+                await player[16].getProperty("innerText")
+              ).jsonValue();
+              const formatPlayer = {
+                name,
+                bw: parseFloat(bw.replace(",", ".")),
+                snatches: await formatLifts(player.slice(6, 9)),
+                cjs: await formatLifts(player.slice(10, 13)),
+                sex: sex.includes("M") ? "M" : "F",
+              };
+
+              const lastIndex = data.length - 1;
+              data[lastIndex].players.push(formatPlayer);
+            }
+          });
       }
     }
   }
-  
+
   await browser.close();
 
-
-
-  return NextResponse.json(data)
+  return NextResponse.json(data);
 }
